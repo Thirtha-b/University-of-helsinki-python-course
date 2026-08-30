c619ac44c9f722910af60b65941b93f684848006
Thirtha <thirthababy59085908@gmail.com>
2026-08-30 23:15:53 +0530

    add name and age exercise

--- part01-14_name_and_age/src/name_and_age.py
+++ part01-14_name_and_age/src/name_and_age.py
@@ -1,1 +1,4 @@
-# Write your solution here
+name=input("Whats is your name?")
+input_str=input("Which year were you born")
+year=int(input_str)
+print(f"Hi {name}, you will be {2021-year} years old at the end of the year 2021")
