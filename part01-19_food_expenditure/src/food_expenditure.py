3564ff89dd4d6b89d96991837f4c2c8a8f37774f
Thirtha <thirthababy59085908@gmail.com>
2026-09-01 00:45:51 +0530

    add food and expenditure exercise

--- part01-19_food_expenditure/src/food_expenditure.py
+++ part01-19_food_expenditure/src/food_expenditure.py
@@ -1,1 +1,8 @@
-# Write your solution here
+cafe=int(input("How mant times a week do you eat at the student cafeteria?"))
+price=float(input("The price of a typical student lunch?"))
+groceries=float(input("How much money do you spend on groceries in a week?"))
+print("Average food expenditure:")
+weekly = cafe*price+groceries
+daily = weekly/7
+print(f"Daily: {daily} euros")
+print(f"Weekly: {weekly} euros")
