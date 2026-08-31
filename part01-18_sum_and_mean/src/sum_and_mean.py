7afe04110f7548f45d5cb3430fba01cbb295bad5
Thirtha <thirthababy59085908@gmail.com>
2026-09-01 00:21:55 +0530

    add sum and mean exercise

--- part01-18_sum_and_mean/src/sum_and_mean.py
+++ part01-18_sum_and_mean/src/sum_and_mean.py
@@ -1,1 +1,7 @@
-# Write your solution here
+num=int(input("write 1 num"))
+num1=int(input("write 2 num"))
+num2=int(input("write 3 num"))
+num3=int(input("write 4 num"))
+total=num+num1+num2+num3
+mean=total/4
+print("The sum of the numbers is",total,"and the mean is",mean)
