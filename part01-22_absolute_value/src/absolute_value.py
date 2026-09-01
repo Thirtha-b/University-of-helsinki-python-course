af4d23f67cb1b1ff748be803925c8aa43dc1235d
Thirtha <thirthababy59085908@gmail.com>
2026-09-01 22:30:54 +0530

    add absolute value exercise

--- part01-22_absolute_value/src/absolute_value.py
+++ part01-22_absolute_value/src/absolute_value.py
@@ -1,1 +1,5 @@
-# Write your solution here
+num1=int(input("Please type a number:"))
+if num1 < 0:
+    print(f"The absolute value of this number is {num1*-1}")
+else:
+    print(f"The absolute value of this number is {num1}")
