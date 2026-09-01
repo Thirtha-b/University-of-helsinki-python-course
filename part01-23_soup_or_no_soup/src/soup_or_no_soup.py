56d9109d7a38169752d219dae88c1897adb5553b
Thirtha <thirthababy59085908@gmail.com>
2026-09-01 23:06:06 +0530

    add soup no soup exercise

--- part01-23_soup_or_no_soup/src/soup_or_no_soup.py
+++ part01-23_soup_or_no_soup/src/soup_or_no_soup.py
@@ -1,1 +1,6 @@
-# Write your solution here
+name=input("Please tell me your name:")
+if name !="Jerry":
+ portion=int(input("How many portions of soup?:"))
+ total_cost=5.90*portion
+ print(f"The total cost is {total_cost}")
+print("Next please!")
