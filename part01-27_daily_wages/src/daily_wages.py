68b67a69380b0977e4424747b5e3e60a8d9a334d
Thirtha <thirthababy59085908@gmail.com>
2026-09-04 23:57:07 +0530

    add daily wages exercise

--- part01-27_daily_wages/src/daily_wages.py
+++ part01-27_daily_wages/src/daily_wages.py
@@ -1,1 +1,7 @@
-# Write your solution here
+wage=float(input("Hourly wage: "))
+hours=int(input("Hours worked: "))
+Day_of_the_week=input("Day of the week: ")
+if Day_of_the_week == "Sunday":
+    print(f"Daily wages: {(2*wage)*hours} euros")
+else:
+    print(f"Daily wages: {wage*hours} euros")
