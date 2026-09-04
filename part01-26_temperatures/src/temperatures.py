72d8d0445bf8565ebb2e7f806b39e4dde8fc19e4
Thirtha <thirthababy59085908@gmail.com>
2026-09-04 22:06:14 +0530

    add temperature exercise

--- part01-26_temperatures/src/temperatures.py
+++ part01-26_temperatures/src/temperatures.py
@@ -1,1 +1,6 @@
-# Write your solution here
+temp=int(input("Please type in a temperature (F):"))
+if temp >0:
+    print(f"{temp} degrees Fahrenheit equals {(temp-32)*5/9} degrees Celsius")
+else: 
+    print(f"{temp} degrees Fahrenheit equals {(temp-32)*5/9} degrees Celsius")
+    print("Brr! It's cold in here!")
