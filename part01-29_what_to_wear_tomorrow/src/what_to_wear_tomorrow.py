88eca5301a0aa7ffb7b5edd7feaa0e1b05c499bb
Thirtha <thirthababy59085908@gmail.com>
2026-09-05 22:22:38 +0530

    add what to wear tomorrow exercise

--- part01-29_what_to_wear_tomorrow/src/what_to_wear_tomorrow.py
+++ part01-29_what_to_wear_tomorrow/src/what_to_wear_tomorrow.py
@@ -1,1 +1,13 @@
-# Write your solution here
+print("What is weather forcastt for tomorrow?")
+Temperature=int(input("Temperature: "))
+rain=input("Will it rain (yes/no): ")
+print("Wear jeans and a T-shirt")
+if Temperature <= 20:
+    print("I recommend a jumper as well")
+if Temperature <= 10:
+    print("Take a jacket with you")
+if Temperature <= 5:
+    print("Make it a warm coat, actually")
+    print("I think gloves are in order")
+if rain == "yes":
+    print("Don't forget your umbrella!")
