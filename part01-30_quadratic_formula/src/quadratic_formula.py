797763958350a593fc7c388abf5ced727f0584d1
Thirtha <thirthababy59085908@gmail.com>
2026-09-06 23:21:06 +0530

    add quadratic formula

--- part01-30_quadratic_formula/src/quadratic_formula.py
+++ part01-30_quadratic_formula/src/quadratic_formula.py
@@ -1,6 +1,12 @@
 # Write your solution here
 # Let's take the square root of math-module in use
-from math import sqrt
+from cmath import sqrt
+a=int(input("enter value"))
+b=int(input("enter value"))
+c=int(input("enter value"))
+root1 = (-b + sqrt(b**2 - 4*a*c)) / (2*a)
+root2 = (-b - sqrt(b**2 - 4*a*c)) / (2*a)
+print(f"The roots are {root1} and {root2}")
 
 # Note that the square root can also be calculated using power.
 # sqrt(9) is equivalent to 9 ** 0.5
