d0c39b5eed7fb760b9310f27d0fdfffedcaac8fa
Thirtha <thirthababy59085908@gmail.com>
2026-09-08 21:50:01 +0530

    add elder exercise

--- part02-06_elder/src/elder.py
+++ part02-06_elder/src/elder.py
@@ -1,1 +1,10 @@
-# Write your solution here
+name1=input("enter name:")
+age1=int(input("enter age:"))
+name2=input("enter name:")
+age2=int(input("enter age:"))
+if age1 > age2:
+    print(f"The elder is {name1}")
+elif age1 < age2:
+    print(f"The elder is {name2}")
+elif age1==age2:
+    print(f"{name1} and {name2} are the same age")
