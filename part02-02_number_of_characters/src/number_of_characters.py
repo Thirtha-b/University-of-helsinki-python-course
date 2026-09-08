71490bf8bbf2b7f08265daa55d4b7cf108b005b9
Thirtha <thirthababy59085908@gmail.com>
2026-09-08 18:48:18 +0530

    add number of characters exercise

--- part02-02_number_of_characters/src/number_of_characters.py
+++ part02-02_number_of_characters/src/number_of_characters.py
@@ -1,1 +1,6 @@
-# Write your solution here
+word=input("Please type in a word:")
+if len(word) > 1:
+    print(f"There are {len(word)} leters in the word {word}")
+    print("Thank you!")
+else:
+    print("Thank you!")
