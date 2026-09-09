70b5379484322bc291ac07e18f33120616650093
Thirtha <thirthababy59085908@gmail.com>
2026-09-09 22:52:00 +0530

    add fizzbuzz exercise

--- part02-11_fizzbuzz/src/fizzbuzz.py
+++ part02-11_fizzbuzz/src/fizzbuzz.py
@@ -1,1 +1,7 @@
-# Write your solution here
+number=int(input("Enter a number:"))
+if number % 3 == 0 and number % 5 == 0:
+    print("FizzBuzz")
+elif number % 3 == 0:
+    print("Fizz")
+elif number % 5 == 0:
+    print("Buzz")
