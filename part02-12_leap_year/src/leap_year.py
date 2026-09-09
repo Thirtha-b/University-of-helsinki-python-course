5637304d74e45792073cd43c3536fc0f678177cb
Thirtha <thirthababy59085908@gmail.com>
2026-09-09 23:34:47 +0530

    add leap year exercise

--- part02-12_leap_year/src/leap_year.py
+++ part02-12_leap_year/src/leap_year.py
@@ -1,1 +1,9 @@
-# Write your solution here
+year=int(input("Enter year:"))
+if year % 400 == 0:
+    print("That year is a leap year.")
+elif year % 100 == 0:
+    print("That year is not a leap year.")
+elif year % 4 == 0:
+    print("That year is a leap year.")
+else:
+    print("That year is not a leap year.")
