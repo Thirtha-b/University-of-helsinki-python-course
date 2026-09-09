8cfac6684b234de0c646d36a703a0f1df1094064
Thirtha <thirthababy59085908@gmail.com>
2026-09-09 21:50:50 +0530

    add grades and points exercise

--- part02-10_grades_and_points/src/grades_and_points.py
+++ part02-10_grades_and_points/src/grades_and_points.py
@@ -1,1 +1,17 @@
-# Write your solution here
+mark=int(input("How many points [0-100]:"))
+if mark < 0:
+    print("Grade: impossible!")
+elif 0 <= mark <= 49:
+    print("Grade: fail")
+elif 50 <= mark <= 59:
+    print("Grade: 1")
+elif 60 <= mark <= 69:
+    print("Grade: 2")
+elif 70 <= mark <= 79:
+    print("Grade: 3")
+elif 80 <= mark <= 89:
+    print("Grade: 4")
+elif 90 <= mark <= 100:
+    print("Grade: 5")
+elif mark > 100:
+    print("Grade: impossible!")
