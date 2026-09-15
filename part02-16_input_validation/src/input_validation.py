11da507a92d5aee80173736cb706f72d05cbd3ab
Thirtha <thirthababy59085908@gmail.com>
2026-09-15 23:12:18 +0530

    add input  validation exercise

--- part02-16_input_validation/src/input_validation.py
+++ part02-16_input_validation/src/input_validation.py
@@ -1,2 +1,12 @@
 from math import sqrt
-# Write your solution here
+
+while True:
+    num = int(input("Enter an integer: "))
+
+    if num < 0:
+        print("Invalid number")
+    elif num == 0:
+        print("Exiting...")
+        break
+    else:
+        print(sqrt(num))
