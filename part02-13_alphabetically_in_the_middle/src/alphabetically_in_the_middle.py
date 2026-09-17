a014a6de82b08746ce9a00bcb414a015ab2a8c6e
Thirtha <thirthababy59085908@gmail.com>
2026-09-17 23:42:47 +0530

    add alphabet exercise

--- part02-13_alphabetically_in_the_middle/src/alphabetically_in_the_middle.py
+++ part02-13_alphabetically_in_the_middle/src/alphabetically_in_the_middle.py
@@ -1,6 +1,6 @@
 letter1 = input("1st letter: ")
 letter2 = input("2nd letter: ")
-letter3 = input("3rd letter: ")
-letters=[letter1,letter2,letter3]
+letter31 = input("3rd letter: ")
+letters=[letter1,letter2,letter31]
 letters.sort()
 print("The letter in the middle is", letters[1])
