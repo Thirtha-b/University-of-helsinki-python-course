9811d21b4e45121f85a27c895f641467757d8f8a
Thirtha <thirthababy59085908@gmail.com>
2026-09-20 17:04:20 +0530

    add pin and number exercise

--- part02-19_pin_and_number_of_attempts/src/pin_and_number_of_attempts.py
+++ part02-19_pin_and_number_of_attempts/src/pin_and_number_of_attempts.py
@@ -1,1 +1,14 @@
-# Write your solution here
+attempts = 0
+
+while True:
+    PIN = int(input("Enter pin: "))
+    attempts += 1
+    
+    if PIN == 4321:
+        if attempts == 1:
+            print("Correct! It only took you one single attempt!")
+        else:
+            print(f"Correct! It took you {attempts} attempts")
+        break
+    else:
+        print("Wrong")
