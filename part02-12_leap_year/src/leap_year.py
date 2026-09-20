af9754b53e08f5c4e1de84da2fe2eeeb93f1803f
Thirtha <thirthababy59085908@gmail.com>
2026-09-20 18:19:36 +0530

    add leap year (way 2nd of solving) exercise

--- part02-12_leap_year/src/leap_year.py
+++ part02-12_leap_year/src/leap_year.py
@@ -1,9 +1,9 @@
 year=int(input("Enter year:"))
-if year % 400 == 0:
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
     print("That year is a leap year.")
-elif year % 100 == 0:
+elif (year % 4 != 0 and year % 100 == 0) or (year % 400 != 0):
     print("That year is not a leap year.")
-elif year % 4 == 0:
+elif (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
     print("That year is a leap year.")
 else:
     print("That year is not a leap year.")
