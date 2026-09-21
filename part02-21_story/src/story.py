1ecd40ec2d50ce192e6a4a191a073b8e00389203
Thirtha <thirthababy59085908@gmail.com>
2026-09-21 21:28:34 +0530

    ahh story to exercise

--- part02-21_story/src/story.py
+++ part02-21_story/src/story.py
@@ -1,1 +1,9 @@
-# Write your solution here
+sentence=""
+previous = ""
+while True:
+    word = input("Please type in a word:")
+    if word == "end" or word ==  previous:
+      break
+    sentence = sentence + word + " "
+    previous = word
+print(sentence)
